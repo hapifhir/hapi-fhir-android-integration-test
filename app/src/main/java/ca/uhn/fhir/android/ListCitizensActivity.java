6f7b44f1a644cbf70c85b99e72f3535b0b598022
JP Ventura <contact@jpventura.com>
2020-05-24 12:41:22 -0300

    refactor(app): android jetpack v1.1.0

--- app/src/main/java/ca/uhn/fhir/android/ListCitizensActivity.java
+++ app/src/main/java/ca/uhn/fhir/android/ListCitizensActivity.java
@@ -2,13 +2,15 @@ package ca.uhn.fhir.android;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
